1e82a4db47454eb58030c993e0ec529d0f517d7d
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2022-06-27 22:22:55 -0400

    feat(ui): now shows query data into tabular format

--- tp1_3.3.py
+++ tp1_3.3.py
@@ -21,11 +21,7 @@ def listar_5():
         tuple(params)
     )
     rows = cursor.fetchall()
-    cols_names = ['ID', 'ASIN', 'CUSTOMER ID', 'REVIEW DATE', 'RATING', 'HELPFUL']
-    print('\t\t\t\t'.join(cols_names))
-    for row in rows:
-        print('\t\t\t\t'.join([str(x) for x in row]))
-    print('')
+    print_table_data(['ID', 'ASIN', 'CUSTOMER ID', 'REVIEW DATE', 'RATING', 'HELPFUL'], [8, 11, 12, 10, 10, 5], rows)
 
 
 def listar_similares():
@@ -39,11 +35,7 @@ def listar_similares():
         tuple(params)
     )
     rows = cursor.fetchall()
-    cols_names = ['TITULO PRODUTO', 'SALESRANK', 'TITULO SIMILAR', 'SALESRANK']
-    print('\t\t\t'.join(cols_names))
-    for row in rows:
-        print('\t\t\t'.join([str(x) for x in row]))
-    print('')
+    print_table_data(['TITULO PRODUTO', 'SALESRANK', 'TITULO SIMILAR', 'SALESRANK'], [80, 12, 80, 12], rows)
 
 
 def listar_evolucao():
@@ -53,14 +45,32 @@ def listar_evolucao():
     conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
     cursor = conn.cursor()
     cursor.execute(
-        f'SELECT product.title, review.review_date, avg(review.rating) FROM product INNER JOIN review ON product.product_id=review.product_id WHERE product.product_id=%s GROUP BY product.title, review.review_date ORDER BY review_date ASC',
+        f'SELECT product.title, review.review_date, round(avg(review.rating), 2) FROM product INNER JOIN review ON product.product_id=review.product_id WHERE product.product_id=%s GROUP BY product.title, review.review_date ORDER BY review_date ASC',
         tuple(params)
     )
     rows = cursor.fetchall()
-    cols_names = ['TITULO', 'DATA', 'MÉDIA DE AVALIAÇÕES']
-    print('\t\t'.join(cols_names))
-    for row in rows:
-        print('\t'.join([str(x) for x in row]))
+    print_table_data(['TITULO', 'DATA', 'MÉDIA DE AVALIAÇÕES'], [150, 14, 10], rows)
+
+
+def listar_mvendidos():
+    print('\t\t\t\tPRODUTOS MAIS VENDIDOS POR GRUPO')
+    conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
+    cursor = conn.cursor()
+    cursor.execute(
+        f'SELECT title, salesrank, product_group FROM (SELECT title, salesrank, product_group, Rank() over (Partition BY product_group ORDER BY salesrank DESC ) AS Rank FROM product WHERE salesrank > 0) rs WHERE Rank <= 10'
+    )
+    rows = cursor.fetchall()
+    print_table_data(['TITULO', 'RANK VENDAS', 'GRUPO'], [150, 15, 13], rows)
+
+
+def print_table_data(header, cols_size, data):
+    for c, l in zip(header, cols_size):
+        print(f'{c.ljust(l)}', end='')
+    print('')
+    for row in data:
+        for c, l in zip(row, cols_size):
+            print(f'{str(c).ljust(l)}', end='')
+        print('')
     print('')
 
 
@@ -71,7 +81,7 @@ if __name__ == '__main__':
         print('[1] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[2] - LISTAR OS PRODUTOS SIMILARES COM MAIORES VENDAS')
         print('[3] - MOSTRAR EVOLUÇÃO DIÁRIA DAS AVALIAÇÕES')
-        print('[4] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
+        print('[4] - LISTAR OS 10 MAIS VENDIDOS EM CADA GRUPO DE PRODUTOS')
         print('[5] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[0] - SAIR')
         op = int(input().strip())
@@ -81,3 +91,5 @@ if __name__ == '__main__':
             listar_similares()
         elif op == 3:
             listar_evolucao()
+        elif op == 4:
+            listar_mvendidos()
