0f573642c3e75b15939c072fa4f031f80f6899e5
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2022-06-27 21:22:26 -0400

    feat(dashboard): add query 7.c

--- tp1_3.3.py
+++ tp1_3.3.py
@@ -12,12 +12,12 @@ from database import DatabaseManager
 
 def listar_5():
     print('INFORME O ID DO PRODUTO:')
-    asin = input()
-    params = (asin, asin)
+    id = input()
+    params = (id, id)
     conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
     cursor = conn.cursor()
     cursor.execute(
-        f'((SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating DESC, helpful DESC LIMIT 5) UNION ALL (SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating ASC, helpful DESC LIMIT 5));',
+        f'((SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating DESC, helpful DESC LIMIT 5) UNION ALL (SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating ASC, helpful DESC LIMIT 5))',
         tuple(params)
     )
     rows = cursor.fetchall()
@@ -29,7 +29,39 @@ def listar_5():
 
 
 def listar_similares():
-    pass
+    print('INFORME O ID DO PRODUTO:')
+    id = input()
+    params = (id,)
+    conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
+    cursor = conn.cursor()
+    cursor.execute(
+        f'SELECT product.title, product.salesrank, psimilar.title, psimilar.salesrank FROM product JOIN similar_products ON product.asin=similar_products.product_asin JOIN product as psimilar ON psimilar.asin=similar_products.similar_asin WHERE product.product_id=%s AND psimilar.salesrank>product.salesrank',
+        tuple(params)
+    )
+    rows = cursor.fetchall()
+    cols_names = ['TITULO PRODUTO', 'SALESRANK', 'TITULO SIMILAR', 'SALESRANK']
+    print('\t\t\t'.join(cols_names))
+    for row in rows:
+        print('\t\t\t'.join([str(x) for x in row]))
+    print('')
+
+
+def listar_evolucao():
+    print('INFORME O ID DO PRODUTO:')
+    id = input()
+    params = (id,)
+    conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
+    cursor = conn.cursor()
+    cursor.execute(
+        f'SELECT product.title, review.review_date, avg(review.rating) FROM product INNER JOIN review ON product.product_id=review.product_id WHERE product.product_id=%s GROUP BY product.title, review.review_date ORDER BY review_date ASC',
+        tuple(params)
+    )
+    rows = cursor.fetchall()
+    cols_names = ['TITULO', 'DATA', 'MÉDIA DE AVALIAÇÕES']
+    print('\t\t'.join(cols_names))
+    for row in rows:
+        print('\t'.join([str(x) for x in row]))
+    print('')
 
 
 if __name__ == '__main__':
@@ -37,8 +69,8 @@ if __name__ == '__main__':
     while op:
         print('SELECIONE UMA DAS OPÇÕES:')
         print('[1] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
-        print('[2] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
-        print('[3] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
+        print('[2] - LISTAR OS PRODUTOS SIMILARES COM MAIORES VENDAS')
+        print('[3] - MOSTRAR EVOLUÇÃO DIÁRIA DAS AVALIAÇÕES')
         print('[4] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[5] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[0] - SAIR')
@@ -47,3 +79,5 @@ if __name__ == '__main__':
             listar_5()
         elif op == 2:
             listar_similares()
+        elif op == 3:
+            listar_evolucao()
