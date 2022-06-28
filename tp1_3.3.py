c0731180fc9a19f562aeaa68d179637eae896631
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2022-06-27 20:59:14 -0400

    feat(dashboard): add tp1 dashboard requeriment (a)

--- tp1_3.3.py
+++ tp1_3.3.py
@@ -10,6 +10,28 @@
 from database import DatabaseManager
 
 
+def listar_5():
+    print('INFORME O ID DO PRODUTO:')
+    asin = input()
+    params = (asin, asin)
+    conn = DatabaseManager.get_connection(DatabaseManager.POSTGRESQL_DB)
+    cursor = conn.cursor()
+    cursor.execute(
+        f'((SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating DESC, helpful DESC LIMIT 5) UNION ALL (SELECT product.product_id, product.asin, review.customer_id, review.review_date, review.rating, review.helpful FROM product, review WHERE product.product_id=%s ORDER BY rating ASC, helpful DESC LIMIT 5));',
+        tuple(params)
+    )
+    rows = cursor.fetchall()
+    cols_names = ['ID', 'ASIN', 'CUSTOMER ID', 'REVIEW DATE', 'RATING', 'HELPFUL']
+    print('\t\t\t\t'.join(cols_names))
+    for row in rows:
+        print('\t\t\t\t'.join([str(x) for x in row]))
+    print('')
+
+
+def listar_similares():
+    pass
+
+
 if __name__ == '__main__':
     op = 99
     while op:
@@ -20,3 +42,8 @@ if __name__ == '__main__':
         print('[4] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[5] - LISTAR OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MAIOR AVALIAÇÃO E OS 5 COMENTÁRIOS MAIS ÚTEIS E COM MENOR AVALIAÇÃO')
         print('[0] - SAIR')
+        op = int(input().strip())
+        if op == 1:
+            listar_5()
+        elif op == 2:
+            listar_similares()
